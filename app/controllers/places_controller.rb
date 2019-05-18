b40c9801f4193fd48ac2c8c4575336e02431d84c
Teddy <tcastro@sandiego.edu>
2019-05-18 13:28:49 +0000

    Nomster_14

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,15 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
   def index
     @places = Place.all.order("name").paginate(page: params[:page], per_page: 1)
   end
