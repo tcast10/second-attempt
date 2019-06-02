29f12af7ce360a0be6eca401dee9fdb89db9f1ea
Teddy <tcastro@sandiego.edu>
2019-06-02 18:06:51 +0000

    Nomster 39 Potential

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,8 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params)
-   # @place.photos.create(photo_params.merge(user: current_user))
+    # @place.photos.create(photo_params)
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
